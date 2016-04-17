7587cacd608e1c4fb005ab7891fde6a4e313743e
Alexander Richey <alexander.richey@gmail.com>
2016-04-17 18:30:25 -0400

    finish game

--- bundle.js
+++ bundle.js
@@ -46,7 +46,7 @@
 
 	var Util = __webpack_require__(1),
 	    Game = __webpack_require__(2),
-	    GameView = __webpack_require__(5);
+	    GameView = __webpack_require__(6);
 
 	document.addEventListener('DOMContentLoaded', function () {
 	  var canvas = document.getElementById('canvas');
@@ -56,12 +56,16 @@
 
 	  var game = new Game(skierSpriteMap, obstacleSpriteMap);
 	  var gameView = new GameView(game, ctx);
-	  canvas.addEventListener('mousemove', function(e){
-	    game.skier.pos[0] = e.clientX;
-	    game.skier.pos[1] = e.clientY;
+	  canvas.addEventListener('mousemove', function(e) {
+	    game.mousePos[0] = e.clientX - canvas.offsetLeft;
+	    game.mousePos[1] = e.clientY;
 	  });
 
 	  gameView.start();
+
+	  canvas.addEventListener('click', function (e) {
+	    gameView.reset();
+	  });
 	});
 
 
@@ -75,15 +79,6 @@
 	    Surrogate.constructor = childClass;
 	    Surrogate.prototype = parentClass.prototype;
 	    childClass.prototype = new Surrogate();
-	  },
-	  randVel: function () {
-	    var vel = [];
-
-	    for (var i = 0; i < 2; i++) {
-	      vel.push(Math.floor(Math.random() * 10));
-	    }
-
-	    return [0, -10];
 	  },
 	  randPos: function () {
 	    var pos = [];
@@ -100,23 +95,6 @@
 	    pos.push(500);
 	    return pos;
 	  },
-	  wrapPos: function (pos) {
-	    if (pos[0] > 500) {
-	      pos[0] = 0;
-	    }
-
-	    if (pos[1] > 500) {
-	      pos[1] = 0;
-	    }
-
-	    if (pos[0] < 0) {
-	      pos[0] = 500;
-	    }
-
-	    if (pos[1] < 0) {
-	      pos[1] = 500;
-	    }
-	  }
 	};
 
 	module.exports = Util;
@@ -128,7 +106,8 @@
 
 	var Util = __webpack_require__(1),
 	    Skier = __webpack_require__(3),
-	    Tree = __webpack_require__(4);
+	    Tree = __webpack_require__(4),
+	    Yeti = __webpack_require__(5);
 
 	var Game = function (skierSpriteMap, obstacleSpriteMap) {
 	  this.skierSpriteMap = skierSpriteMap;
@@ -137,28 +116,40 @@
 	  NUM_TREES = 5;
 	  this.trees = [];
 
-	  this.skier = new Skier([250, 250], this.skierSpriteMap);
-	  this.addTrees(this.obstacleSpriteMap);
+	  this.skier = new Skier(this.skierSpriteMap);
+	  this.yeti = new Yeti(this.skierSpriteMap);
+	  this.addTrees();
+
+	  this.paused = false;
+	  this.score = 0;
+	  this.crashCount = 0;
+	  this.isSkierCaught = false;
+
+	  this.mousePos = [250, 250];
+	};
+
+	Game.prototype.reset = function () {
+	  NUM_TREES = 5;
+	  this.trees = [];
+
+	  this.skier = new Skier(this.skierSpriteMap);
+	  this.yeti = new Yeti(this.skierSpriteMap);
+	  this.addTrees();
+
+	  this.paused = false;
+	  this.score = 0;
+	  this.crashCount = 0;
+	  this.isSkierCaught = false;
+
+	  this.mousePos = [250, 250];
 	};
 
 	Game.prototype.addTrees = function (spriteMap) {
 	  for (var i = 0; i < NUM_TREES; i++) {
 	    this.trees.push(
-	      new Tree(Util.randPos(), spriteMap)
+	      new Tree(Util.randPos(), this.obstacleSpriteMap)
 	    );
 	  }
-	};
-
-	Game.prototype.checkCollision = function () {
-	  this.trees.forEach(function (tree) {
-	    if (
-	      Math.sqrt(
-	        Math.pow((tree.pos[0] - tree.pos[1]), 2) +
-	        Math.pow((this.skier.pos[0] - this.skier.pos[1]), 2)
-	      ) < 10) {
-	      console.log('COLLISION');
-	    }
-	  }.bind(this));
 	};
 
 	Game.prototype.removeElapsed = function () {
@@ -188,26 +179,134 @@
 	  }
 	};
 
+	Game.prototype.checkCollision = function () {
+	  for (var i = 0; i < this.trees.length; i++) {
+	    if (
+	        (
+	          Math.sqrt(
+	          Math.pow((this.trees[i].pos[0] - this.skier.pos[0]), 2) +
+	          Math.pow((this.trees[i].pos[1] - this.skier.pos[1]), 2)
+	          ) < 20
+	        ) &&
+	        this.trees[i].colided === false
+	      ) {
+	      this.colide(this.trees[i]);
+	      return;
+	    } else {
+	      this.skier.ok();
+	    }
+	  }
+	};
+
+	Game.prototype.colide = function (obstacle) {
+	  this.skier.colide();
+	  obstacle.colided = true;
+	  this.paused = true;
+	  this.crashCount += 1;
+	  this.score -= 100;
+	  setTimeout(function () {
+	    this.paused = false;
+	  }.bind(this), 1000);
+	};
+
+	Game.prototype.checkCaught = function () {
+	  if (
+	        Math.sqrt(
+	        Math.pow((this.yeti.pos[0] - this.skier.pos[0]), 2) +
+	        Math.pow((this.yeti.pos[1] - this.skier.pos[1]), 2)
+	        ) < 20
+	      ) {
+	        this.catchSkier();
+	  }
+	};
+
+	Game.prototype.catchSkier = function () {
+	  this.isSkierCaught = true;
+	  this.paused = true;
+	  this.yeti.catchSkier();
+	  this.skier.getCaught();
+	};
+
+	Game.prototype.tallyScore = function () {
+	  this.score += 1;
+
+	  if (this.score > 1000) {
+	    NUM_TREES = 25;
+	  } else if (this.score > 750) {
+	    NUM_TREES = 11;
+	  } else if (this.score > 500) {
+	    NUM_TREES = 8;
+	  } else {
+	    NUM_TREES = 5;
+	  }
+	};
+
+	Game.prototype.drawStats = function (ctx) {
+	  var score = "Score: " + this.score;
+	  var crash = "Crashes: " + this.crashCount;
+
+	  ctx.font = "18px 'PT Sans'";
+	  ctx.fillText(score, 10, 20);
+	  ctx.fillText(crash, 120, 20);
+
+	  this.gameOverMessage(ctx);
+	};
+
+	Game.prototype.gameOverMessage = function (ctx) {
+	  if (this.isSkierCaught) {
+	    ctx.font = "40px 'PT Sans'";
+	    var gameOverMessage = "You are Yeti food";
+	    var gameOverTextWidth = ctx.measureText(gameOverMessage).width;
+	    ctx.fillText(
+	      gameOverMessage,
+	      (canvas.width/2) - (gameOverTextWidth / 2),
+	      100
+	    );
+
+	    ctx.font = "18px 'PT Sans'";
+	    var playAgainMessage = "Click to play again";
+	    var playAgainTextWidth = ctx.measureText(playAgainMessage).width;
+	    ctx.fillText(
+	      playAgainMessage,
+	      (canvas.width/2) - (playAgainTextWidth / 2),
+	      400
+	    );
+	  }
+	};
+
 	Game.prototype.moveObjects = function () {
+	  this.skier.move(this.mousePos);
+	  this.yeti.move(this.skier.pos);
 	  this.trees.forEach(function (tree) {
 	    tree.move();
 	  });
 	};
 
 	Game.prototype.step = function () {
-	  this.rebalanceTrees();
-	  this.checkCollision();
-	  this.moveObjects();
+	  if (this.isSkierCaught) {
+	    this.paused = true;
+	  }
+	  if (!this.paused) {
+	    this.moveObjects();
+	    this.tallyScore();
+	    this.rebalanceTrees();
+	    this.checkCollision();
+	    this.checkCaught();
+	  } else {
+	    this.yeti.catchUp();
+	    this.checkCaught();
+	  }
 	};
 
 	Game.prototype.draw = function (ctx) {
 	  ctx.clearRect(0, 0, 500, 500);
 
-	  this.skier.draw(ctx);
-
 	  this.trees.forEach(function (tree) {
 	    tree.draw(ctx);
 	  });
+	  this.skier.draw(ctx);
+	  this.yeti.draw(ctx);
+	  this.drawStats(ctx);
 	};
 
 	module.exports = Game;
@@ -217,19 +316,97 @@
 /* 3 */
 /***/ function(module, exports) {
 
-	var Skier = function (pos, spriteMap) {
+	var Skier = function (spriteMap) {
 	  this.spriteMap = spriteMap;
 	  this.radius = 20;
 	  this.color = 'red';
-	  this.pos = pos;
+	  this.pos = [250, 250];
+	  this.state = "OK";
+	  this.vel = 0;
+	  this.isCaught = false;
+	};
+
+	Skier.prototype.getCaught = function () {
+	  this.isCaught = true;
+	};
+
+	Skier.prototype.colide = function () {
+	  this.state = "COLIDE";
+	};
+
+	Skier.prototype.ok = function () {
+	  this.state = "OK";
+	};
+
+	Skier.prototype.isColide = function () {
+	  if (this.state === "COLIDE") {
+	    return true;
+	  } else {
+	    return false;
+	  }
+	};
+
+	Skier.prototype.isOk = function () {
+	  if (this.state === "OK") {
+	    return true;
+	  } else {
+	    return false;
+	  }
+	};
+
+	Skier.prototype.pushLeft = function () {
+	  this.vel += 0.2;
+	};
+
+	Skier.prototype.pushRight = function () {
+	  this.vel -= 0.2;
+	};
+
+	Skier.prototype.move = function (newPos) {
+	  if (newPos[0] > this.pos[0]) {
+	    this.pushLeft();
+	  } else if (newPos[0] < this.pos[0]) {
+	    this.pushRight();
+	  }
+
+	  if (!(this.pos[0] + this.vel >= 490 || this.pos[0] + this.vel <= 0)) {
+	    this.pos[0] += this.vel;
+	  }
 	};
 
 	Skier.prototype.draw = function (ctx) {
-	  ctx.drawImage(
-	    this.spriteMap,
-	    65, 0, 17, 34,
-	    this.pos[0], this.pos[1], 17, 34
-	  );
+	  if (this.isCaught) { return; }
+
+	  switch (this.state) {
+	    case "OK":
+	      if (this.vel > 0.5) {
+	        ctx.drawImage(
+	          this.spriteMap,
+	          49, 0, 17, 34,
+	          this.pos[0], this.pos[1], 17, 34
+	        );
+	      } else if (this.vel < -0.5) {
+	        ctx.drawImage(
+	          this.spriteMap,
+	          49, 37, 17, 34,
+	          this.pos[0], this.pos[1], 17, 34
+	        );
+	      } else {
+	        ctx.drawImage(
+	          this.spriteMap,
+	          65, 0, 17, 34,
+	          this.pos[0], this.pos[1], 17, 34
+	        );
+	      }
+	      break;
+	    case "COLIDE":
+	      ctx.drawImage(
+	        this.spriteMap,
+	        240, 0, 31, 31,
+	        this.pos[0], this.pos[1], 31, 31
+	      );
+	      break;
+	  }
 	};
 
 	module.exports = Skier;
@@ -243,18 +420,45 @@
 
 	var Tree = function (pos, spriteMap) {
 	  this.spriteMap = spriteMap;
+	  this.type = Math.floor(Math.random() * 5);
 	  this.pos = pos;
-	  this.vel = Util.randVel();
+	  this.vel = [0, -10];
 	  this.radius = 40;
 	  this.color = 'green';
+	  this.colided = false;
 	};
 
 	Tree.prototype.draw = function (ctx) {
-	  ctx.drawImage(
-	    this.spriteMap,
-	    95, 66, 32, 64,
-	    this.pos[0], this.pos[1], 32, 64
-	  );
+	  switch (this.type) {
+	    case 1:
+	      ctx.drawImage(
+	        this.spriteMap,
+	        0, 28, 30, 34,
+	        this.pos[0], this.pos[1], 30, 34
+	      );
+	      break;
+	    case 2:
+	      ctx.drawImage(
+	        this.spriteMap,
+	        95, 66, 32, 64,
+	        this.pos[0], this.pos[1], 32, 64
+	      );
+	      break;
+	    case 3:
+	      ctx.drawImage(
+	        this.spriteMap,
+	        30, 52, 23, 11,
+	        this.pos[0], this.pos[1], 23, 11
+	      );
+	      break;
+	    case 4:
+	      ctx.drawImage(
+	        this.spriteMap,
+	        85, 138, 15, 32,
+	        this.pos[0], this.pos[1], 15, 32
+	      );
+	      break;
+	  }
 	};
 
 	Tree.prototype.move = function () {
@@ -269,6 +473,204 @@
 /* 5 */
 /***/ function(module, exports) {
 
+	var Yeti = function (spriteMap) {
+	  this.spriteMap = spriteMap;
+	  this.pos = [250, -100];
+	  this.frame = 0;
+	  this.frameTimer = 0;
+	  this.vel = 0.05;
+	  this.isCaughtSkier = false;
+	};
+
+	Yeti.prototype.pushLeft = function () {
+	  this.vel += 0.05;
+	};
+
+	Yeti.prototype.pushRight = function () {
+	  this.vel -= 0.05;
+	};
+
+	Yeti.prototype.move = function (newPos) {
+	  if (newPos[0] > this.pos[0]) {
+	    this.pushLeft();
+	  } else if (newPos[0] < this.pos[0]) {
+	    this.pushRight();
+	  }
+
+	  this.pos[0] += this.vel;
+	};
+
+	Yeti.prototype.catchUp = function () {
+	  if (this.isCaughtSkier === true) { return; }
+
+	  this.pos[0] += this.vel;
+
+	  if (this.pos[1] < 250) {
+	    this.pos[1] += 1.5;
+	  }
+	};
+
+	Yeti.prototype.catchSkier = function () {
+	  this.isCaughtSkier = true;
+	};
+
+	Yeti.prototype.incrementTimer = function (callback) {
+	  if (this.frameTimer > 6) {
+	    this.frameTimer = 0;
+
+	    if (callback) {
+	      callback();
+	      return;
+	    }
+
+	    this.switchFrame();
+	  } else {
+	    this.frameTimer += 1;
+	  }
+	};
+
+	Yeti.prototype.switchFrame = function () {
+	  if (this.frame === 0) {
+	    this.frame = 1;
+	  } else if (this.frame === 1) {
+	    this.frame = 0;
+	  } else if (this.frame > 1) {
+	    this.frame = 0;
+	  }
+	};
+
+	Yeti.prototype.draw = function (ctx) {
+	  if (this.isCaughtSkier) {
+	    this.eatSkier(ctx);
+	    return;
+	  }
+
+	  this.incrementTimer();
+
+	  if (this.vel > 0) {
+	    switch (this.frame) {
+	      case 0:
+	        ctx.drawImage(
+	          this.spriteMap,
+	          91, 112, 31, 40,
+	          this.pos[0], this.pos[1], 31, 40
+	        );
+	        break;
+	      case 1:
+	        ctx.drawImage(
+	          this.spriteMap,
+	          62, 112, 29, 40,
+	          this.pos[0], this.pos[1], 29, 40
+	        );
+	        break;
+	    }
+	  } else if (this.vel < 0) {
+	    switch (this.frame) {
+	      case 0:
+	        ctx.drawImage(
+	          this.spriteMap,
+	          91, 158, 31, 40,
+	          this.pos[0], this.pos[1], 31, 40
+	        );
+	        break;
+	      case 1:
+	        ctx.drawImage(
+	          this.spriteMap,
+	          62, 158, 29, 40,
+	          this.pos[0], this.pos[1], 29, 40
+	        );
+	        break;
+	    }
+	  }
+	};
+
+	Yeti.prototype.eatSkier = function (ctx) {
+	  this.incrementTimer(function () {
+	    if (this.frame > 6) {
+	      if (this.frame === 7) {
+	        this.frame = 8;
+	      } else if (this.frame === 8) {
+	        this.frame = 7;
+	      }
+	    } else {
+	      this.frame += 1;
+	    }
+	  }.bind(this));
+
+	  switch (this.frame) {
+	    case 0:
+	      ctx.drawImage(
+	        this.spriteMap,
+	        35, 112, 25, 43,
+	        this.pos[0], this.pos[1], 25, 43
+	      );
+	      break;
+	    case 1:
+	      ctx.drawImage(
+	        this.spriteMap,
+	        0, 112, 32, 43,
+	        this.pos[0], this.pos[1], 32, 43
+	      );
+	      break;
+	    case 2:
+	      ctx.drawImage(
+	        this.spriteMap,
+	        122, 112, 34, 43,
+	        this.pos[0], this.pos[1], 34, 43
+	      );
+	      break;
+	    case 3:
+	      ctx.drawImage(
+	        this.spriteMap,
+	        156, 112, 31, 43,
+	        this.pos[0], this.pos[1], 31, 43
+	      );
+	      break;
+	    case 4:
+	      ctx.drawImage(
+	        this.spriteMap,
+	        187, 112, 31, 43,
+	        this.pos[0], this.pos[1], 31, 43
+	      );
+	      break;
+	    case 5:
+	      ctx.drawImage(
+	        this.spriteMap,
+	        219, 112, 25, 43,
+	        this.pos[0], this.pos[1], 25, 43
+	      );
+	      break;
+	    case 6:
+	      ctx.drawImage(
+	        this.spriteMap,
+	        243, 112, 26, 43,
+	        this.pos[0], this.pos[1], 26, 43
+	      );
+	      break;
+	    case 7:
+	      ctx.drawImage(
+	        this.spriteMap,
+	        35, 112, 25, 43,
+	        this.pos[0], this.pos[1], 25, 43
+	      );
+	      break;
+	    case 8:
+	      ctx.drawImage(
+	        this.spriteMap,
+	        0, 112, 32, 43,
+	        this.pos[0], this.pos[1], 32, 43
+	      );
+	      break;
+	  }
+	};
+
+	module.exports = Yeti;
+
+
+/***/ },
+/* 6 */
+/***/ function(module, exports) {
+
 	var GameView = function (game, ctx) {
 	  this.game = game;
 	  this.ctx = ctx;
@@ -279,6 +681,12 @@
 	    this.game.draw(this.ctx);
 	    this.game.step();
 	  }.bind(this), 30);
+	};
+
+	GameView.prototype.reset = function () {
+	  if (this.game.isSkierCaught) {
+	    this.game.reset();
+	  }
 	};
 
 	module.exports = GameView;
