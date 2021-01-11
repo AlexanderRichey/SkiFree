506272d0d86ba266f7eba657ae6bddd35a642fb3
Alexander Richey <alexander.richey@gmail.com>
2021-01-11 14:26:41 -0500

    recompile skifree so that it runs regardless of page load state

--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,8 @@
 module.exports = {
-    context: __dirname,
-    entry: "./lib/entry",
-    output: {
-        path: "./",
-        filename: "bundle.js"
-    }
+  context: __dirname,
+  entry: "./lib/entry",
+  output: {
+    path: `${__dirname}/.`,
+    filename: "skifree.js",
+  },
 };
